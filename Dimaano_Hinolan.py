418f5c73d1c9d18165eb948b1ad6ef5591810179
Josanine Mariel D. Hinolan <95222721+josaninehinolan@users.noreply.github.com>
2022-07-17 20:49:23 +0800

    Update Dimaano_Hinolan.py

--- Dimaano_Hinolan.py
+++ Dimaano_Hinolan.py
@@ -1,6 +1,59 @@
+# This is Fan (electric) class 
+
+class Fan: 
+    
+#attributes
+    def __init__(self, n, t, c, dj, m, p):
+        self.name = n
+        self.type = t
+        self.color = c
+        self.state = dj
+        self.mode = m
+        self.speed = p
+
+#methods
+    def give_name(self,n):
+        self.name = n 
+	      
+    def change_shape(self,t):
+        self.type = t
+	      
+    def change_color(self,c):
+        self.color = c 
+	      
+    def change_state(self,dj):
+        self.state = dj
+
+    def change_mode(self,m):
+        self.mode = m
+	      
+    def change_speed(self,p):
+        self.speed = p
+
+fan1 = Fan("Asahi", "ceiling", "green", "ON", "swing", "high")
+fan2 = Fan("Xiaomi", "tower", "pink", "ON", "still", "medium")
+
+print ("Fan brand is",fan1.name)
+print ("Fan type is",fan1.type)
+print ("Fan color is",fan1.color)
+print ("Fan state is",fan1.state)
+print ("Fan mode is",fan1.mode)
+print("Fan speed is",fan1.speed)
+
+print("")
+print("")
+
+print ("Changed brand to",fan2.name)
+print ("Preferred: ",fan2.type,"fan type")
+print ("Changed color to",fan2.color)
+print ("Fan state is",fan2.state)
+print ("Fan mode is",fan2.mode)
+print("Fan speed is",fan2.speed)
+
+
 """
 This is the Lamp class.
-It encompasses the attributes and methos of the object which the enocoders wishes to include.
+It encompasses the attributes and methods of the object which the enocoders wishes to include.
 Some of the lines are copied over or modified from the original code to ease the encoder's life.
 """
 
