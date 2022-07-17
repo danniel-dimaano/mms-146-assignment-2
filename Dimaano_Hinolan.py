72c8f766b01d0a3d8d20c0b5085121cdd6134f95
Josanine Mariel D. Hinolan <95222721+josaninehinolan@users.noreply.github.com>
2022-07-17 19:14:51 +0800

    Update Dimaano_Hinolan.py

--- Dimaano_Hinolan.py
+++ Dimaano_Hinolan.py
@@ -5,4 +5,21 @@ class Lamp(object):
     def __init__(self, arg):
         super(, self).__init__()
         self.arg = arg
+
+# This is Fan (electric) class 
+
+class Fan: 
+    
+    def __init__(self, n, s, c, d, j, p):
+        self.name = n
+        self.shape = s
+        self.color = c
+        self.on = d
+        self.off = j
+        self.speed = p
+    
+    def increase_speed(self):
+		self.speed = self.speed + 1
+
+
         
