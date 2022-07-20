f97faf90a3a56f5dee941396f6dbe88c1ff462e7
Josanine Mariel D. Hinolan <95222721+josaninehinolan@users.noreply.github.com>
2022-07-20 17:50:07 +0800

    Update Dimaano_Hinolan.py

--- Dimaano_Hinolan.py
+++ Dimaano_Hinolan.py
@@ -1,5 +1,19 @@
 # This is Fan (electric) class 
 
+""" 
+Situation:
+
+Danniel plans to purchase an electric fan for his new bedroom; however, he first wants to look at options before deciding. So, to narrow his choices, 
+he settled with two brands: Asahi and Xiaomi. Then, he considered the following: the electric fan's type, color, state, mode, and speed. 
+
+He mainly wanted a ceiling or tower fan as those are space-savers for his bedroom. Then he found the colors green or pink complementary to his room's theme. 
+So, he asked for an Asahi Ceiling fan in the color green and a Xiaomi Tower fan in pink. Then, he turned them on and checked if the Asahi ceiling fan could
+rotate smoothly in 360 degrees when on swing mode enough to cover his bedroom even when on medium speed only. And he turned on the Xiaomi tower fan and placed 
+it at high speed to see if it could cover a vast space even on still mode.
+
+Then, he turned the Asahi and  Xiaomi fans off and observed whether the green ceiling fan or the pink tower fan fit his bedroom the best. 
+"""
+
 class Fan: 
     
 #attributes
@@ -15,7 +29,7 @@ class Fan:
     def give_name(self,n):
         self.name = n 
 	      
-    def change_shape(self,t):
+    def change_type(self,t):
         self.type = t
 	      
     def change_color(self,c):
@@ -30,25 +44,46 @@ class Fan:
     def change_speed(self,p):
         self.speed = p
 
-fan1 = Fan("Asahi", "ceiling", "green", "ON", "swing", "high")
-fan2 = Fan("Xiaomi", "tower", "pink", "ON", "still", "medium")
+fan1 = Fan("Asahi", "Ceiling", "Green", "On", "Swing", "Medium")
+fan2 = Fan("Xiaomi", "Tower", "Pink", "On", "Still", "High")
+fan3 = Fan("Asahi", "Ceiling", "Green", "Off", "None", "None")
+fan4 = Fan("Xiaomi", "Tower", "Pink", "Off", "None", "None")
 
-print ("Fan brand is",fan1.name)
-print ("Fan type is",fan1.type)
-print ("Fan color is",fan1.color)
-print ("Fan state is",fan1.state)
-print ("Fan mode is",fan1.mode)
-print("Fan speed is",fan1.speed)
+print ("Brand:",fan1.name)
+print ("Type:",fan1.type)
+print ("Color:",fan1.color)
+print ("State:",fan1.state)
+print ("Mode:",fan1.mode)
+print ("Speed:",fan1.speed)
+
+print("")
+
+print ("Brand:",fan2.name)
+print ("Type:",fan2.type)
+print ("Color:",fan2.color)
+print ("State:",fan2.state)
+print ("Mode:",fan2.mode)
+print ("Speed:",fan2.speed)
 
 print("")
 print("")
 
-print ("Changed brand to",fan2.name)
-print ("Preferred: ",fan2.type,"fan type")
-print ("Changed color to",fan2.color)
-print ("Fan state is",fan2.state)
-print ("Fan mode is",fan2.mode)
-print("Fan speed is",fan2.speed)
+print ("Brand:",fan3.name)
+print ("Type:",fan3.type)
+print ("Color:",fan3.color)
+print ("State:",fan3.state)
+print ("Mode:",fan3.mode)
+print ("Speed:",fan3.speed)
+
+print("")
+
+print ("Brand:",fan4.name)
+print ("Type:",fan4.type)
+print ("Color:",fan4.color)
+print ("State:",fan4.state)
+print ("Mode:",fan4.mode)
+print ("Speed:",fan4.speed)
+
 
 
 """
