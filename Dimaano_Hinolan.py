977864c8f81fb93b2c27adcf8249616f097c2b86
Danniel <95221990+danniel-dimaano@users.noreply.github.com>
2022-07-22 20:49:23 +0800

    Update Dimaano_Hinolan.py

--- Dimaano_Hinolan.py
+++ Dimaano_Hinolan.py
@@ -1,18 +1,57 @@
+"""
+Danniel and Josanine are an incoming third-year BA in Multimedia Studies students
+from the University of the Philippines - Open University. And since they are online
+distance learning students, they and their group of friends from class decided to
+rent a boarding place where they could each have separate bedrooms, which also
+serve as their personal study spaces. Being in college, they wanted to have a sense
+of independence and figure out things in their life as they grew and matured. 
+
+So, since Danniel and Josanine were the first to move into their new boarding
+place, they decided to go ahead and organize their respective bedrooms. They each
+made a list of what they needed to buy to settle comfortably in their new place. 
+
+With that on Danniel's list, he needed the following for his bedroom: an electric
+fan, bathroom tiles, and a bed. On the other hand, here is the list that Josanine
+made: an LED lamp, an electric iron, and a pair of slippers. In addition, they both
+decided to add something on their list as a welcome gift or contribution to their
+group of friends as they settled into their new place. So, Danniel wanted to give
+each of his friends a custom-made bar of soap, while Josanine decided to contribute
+a refrigerator for them to share. 
+
+Now, Danniel and Josanine are off to a shopping time at various online and physical
+stores to complete what they need for their new place and bedrooms. The challenge
+of this errand is how good will they be in making their choices to pick the right
+items that will best fit their needs and expectations. 
+
+So, keep scrolling to see how these two did on their shopping errand!
+"""
+
+
+
+
 # This is Fan (electric) class 
 
 """ 
-Danniel plans to purchase an electric fan for his new bedroom; however, he first wants to look at options before deciding. So, to narrow his choices, he settled 
-with two brands: Asahi and Xiaomi. Then, he considered the following: the electric fan's type, color, state, mode, and speed. 
+Danniel plans to purchase an electric fan for his new bedroom; however, he first
+wants to look at options before deciding. So, to narrow his choices, he settled
+with two brands: Asahi and Xiaomi. Then, he considered the following: the electric
+fan's type, color, state, mode, and speed. 
 
-He mainly wanted a ceiling or tower fan as those are space-savers for his bedroom. Then he found the colors green or pink complementary to his room's theme. So, 
-he asked for an Asahi Ceiling fan in the color green and a Xiaomi Tower fan in pink. Then, he turned them on and checked if the Asahi ceiling fan could 
-rotate smoothly at 360 degrees when on swing mode enough to cover his bedroom from medium to high speed. And he turned on the Xiaomi tower fan and placed it from 
+He mainly wanted a ceiling or tower fan as those are space-savers for his bedroom.
+Then he found the colors green or pink complementary to his room's theme. So, 
+he asked for an Asahi Ceiling fan in the color green and a Xiaomi Tower fan in
+pink. Then, he turned them on and checked if the Asahi ceiling fan could 
+rotate smoothly at 360 degrees when on swing mode enough to cover his bedroom from
+medium to high speed. And he turned on the Xiaomi tower fan and placed it from
 medium to high speed to see if it could cover a vast space even on still mode.
 
-Then, he turned the Asahi and  Xiaomi fans off and observed whether the green ceiling fan or the pink tower fan fit his bedroom the best. 
+Then, he turned the Asahi and  Xiaomi fans off and observed whether the green
+ceiling fan or the pink tower fan fit his bedroom the best. 
 
-At last, he picked the Xiaomi Tower fan in pink and decided to buy it already for his new bedroom. As he found its speed from medium to high stronger and more 
-refreshing, the tower fan type is also a space saver and could cover a vast area of his room, and the color pink complements the theme of his room very well. 
+At last, he picked the Xiaomi Tower fan in pink and decided to buy it already for
+his new bedroom. As he found its speed from medium to high stronger and more
+refreshing, the tower fan type is also a space saver and could cover a vast area of
+his room, and the color pink complements the theme of his room very well. 
 """
 
 class Fan: 
@@ -84,22 +123,32 @@ print ("Current state:", fan2.state)
 #This is Tile class
 
 """
-For Danniel's bathroom, he wanted to be specific with its tiles to ensure that it is pleasing, durable, and safe. So, he went to an online tile store and found 
-two brand options by Wilcon and Eurotiles. Then, he specified his tile preferences to the store's virtual assistant; he wanted something that was not slippery 
+For Danniel's bathroom, he wanted to be specific with its tiles to ensure that it
+is pleasing, durable, and safe. So, he went to an online tile store and found 
+two brand options by Wilcon and Eurotiles. Then, he specified his tile preferences
+to the store's virtual assistant; he wanted something that was not slippery
 but will look shiny. And he wanted tile shapes that were not too ordinary. 
 
 With that, he was presented with two options for a bathroom-friendly tile: 
 
-First is the Wilcon brand with a porcelain material in the shape of a hexagon with a sandblasted texture, a coastal luster, and 8mm thickness. But he also wanted 
-the same tile features in a slate material, so he was presented with a slate tile material with the same tile design. 
+First is the Wilcon brand with a porcelain material in the shape of a hexagon with
+a sandblasted texture, a coastal luster, and 8mm thickness. But he also wanted
+the same tile features in a slate material, so he was presented with a slate tile
+material with the same tile design. 
 
-On the other hand, Eurotiles offers a slate material in the shape of a chevron with an etched texture but with oyster luster and a thickness of 6mm. So, Danniel 
+On the other hand, Eurotiles offers a slate material in the shape of a chevron with
+an etched texture but with oyster luster and a thickness of 6mm. So, Danniel
 also asked for the Eurotiles' tile design but in the porcelain material. 
 
-Danniel liked the two options he was presented with. But before he decides, he wants to make sure that he gets to see thoroughly on which brand offers the best 
-quality and design, in particular, with the tile material. That is why he also switched the tile materials from Wilcon's porcelain to their slate and Eurotiles' slate
-to their porcelain to see which one has the better deal. So, he ordered tile samples from each brand with his specific preferences to see which one fits best with his 
-bathroom for this new bedroom. From there, once he's sure about a particular choice for his bathroom tiles is when he will order a bunch to start construction.
+Danniel liked the two options he was presented with. But before he decides, he
+wants to make sure that he gets to see thoroughly on which brand offers the best
+quality and design, in particular, with the tile material. That is why he also
+switched the tile materials from Wilcon's porcelain to their slate and Eurotiles'
+slate to their porcelain to see which one has the better deal. So, he ordered tile
+samples from each brand with his specific preferences to see which one fits best
+with his bathroom for this new bedroom. From there, once he's sure about a
+particular choice for his bathroom tiles is when he will order a bunch to start
+construction.
 """
 
 class Tile: 
@@ -167,22 +216,31 @@ print("Thickness:",tile2.thickness)
 #This is Bed class
 
 """
-Another thing that Danniel wants for his bedroom is to meticulously pick a bed that fits him and his standard of comfort. So, he went online shopping again, but this 
-time for a bed. And he had the following things to consider: it must be a single bed for an adult person with a rectangular shape, either metal or wood frame, and in
-a brand new condition with a comfortable mattress to sleep on. 
+Another thing that Danniel wants for his bedroom is to meticulously pick a bed
+that fits him and his standard of comfort. So, he went online shopping again, but
+this time for a bed. And he had the following things to consider: it must be a
+single bed for an adult person with a rectangular shape, either metal or wood
+frame, and in a brand new condition with a comfortable mattress to sleep on. 
 
-The online store offers a wide range of brands to choose from. But he picked the Sleepwell and Wakefit brands for his options since he is already familiar with those 
-two. 
+The online store offers a wide range of brands to choose from. But he picked the
+Sleepwell and Wakefit brands for his options since he is already familiar with
+those two. 
 
-First, the Sleepwell brand offers a brand new adult single bed with a size of 90cm x 190cm in a rectangular shape. However, they only have the bed in a metal bed 
-frame material. So, he checked the two options available: the iron metal and the steel metal frame. And they have a traditional foam mattress that comes with it. 
+First, the Sleepwell brand offers a brand new adult single bed with a size of
+90cm x 190cm in a rectangular shape. However, they only have the bed in a metal bed
+frame material. So, he checked the two options available: the iron metal and the
+steel metal frame. And they have a traditional foam mattress that comes with it. 
 
-And second, the Wakefit brand has the same features as the Sleepwell brand. But they only have it in a wooden bed frame material which he can choose between mahogany 
-or oak wood frames. But it comes with a gel memory foam mattress. 
+And second, the Wakefit brand has the same features as the Sleepwell brand. But
+they only have it in a wooden bed frame material which he can choose between
+mahogany or oak wood frames. But it comes with a gel memory foam mattress. 
 
-Since Danniel finds it crucial to find the most durable and comfortable bed possible, he took his time to decide. He researched his options until he chose the 
-Sleepwell brand with the steel metal bed frame because he discovered that metal steel bed frames are durable yet more affordable, and the Sleepwell brand has a gel 
-memory foam mattress that comes with it. And that is a good quality mattress and bed he has wanted for a while now.
+Since Danniel finds it crucial to find the most durable and comfortable bed
+possible, he took his time to decide. He researched his options until he chose the
+Sleepwell brand with the steel metal bed frame because he discovered that metal
+steel bed frames are durable yet more affordable, and the Sleepwell brand has a gel
+memory foam mattress that comes with it. And that is a good quality mattress and
+bed he has wanted for a while now.
 """
 
 class Bed: 
@@ -261,14 +319,20 @@ print("Shape:",bed2.shape)
 #This is Soap class
 
 """
-To complete his bathroom essentials, Danniel went to the Lush online store to order a customized body soap and gift it to his friends at the boarding place. So he 
-considered the following: the soap's shape, color, fragrance, usage, and lather intensity. 
+To complete his bathroom essentials, Danniel went to the Lush online store to order
+a customized body soap and gift it to his friends at the boarding place. So he
+considered the following: the soap's shape, color, fragrance, usage, and lather
+intensity. 
 
-And as he scrolled, he found a color yellow body soap in a rectangular shape with a lather intensity of seven in a lemon citrus scent. However, he doesn't feel like 
-it is the right one for him and something his friends would enjoy having for a bath. So, he customized the body soap through the Lush website's soap customization 
-featured page and changed the shape to an oval, the color to peach, and its fragrance to an oat milk scent, but kept the lather intensity to seven. 
+And as he scrolled, he found a color yellow body soap in a rectangular shape with a
+lather intensity of seven in a lemon citrus scent. However, he doesn't feel like it
+is the right one for him and something his friends would enjoy having for a bath.
+So, he customized the body soap through the Lush website's soap customization
+featured page and changed the shape to an oval, the color to peach, and its
+fragrance to an oat milk scent, but kept the lather intensity to seven. 
 
-Once satisfied with his choice of soap, he ordered a couple of pieces for himself and his friends. 
+Once satisfied with his choice of soap, he ordered a couple of pieces for himself
+and his friends. 
 """
 
 class Soap: 
@@ -331,9 +395,12 @@ print("Lather Intensity:",soap.lather_intensity)
 
 
 """
-This is the Lamp class.
-It encompasses the attributes and methods of the object which the enocoders wishes to include.
-Some of the lines are copied over or modified from the original code to ease the encoder's life.
+Situation:
+Everyone knows how much of a night owl Josanine has always been. She's a pro at
+staying late at night for study or leisure time, so a good-quality and durable lamp
+is essential for her bedroom. With that, she went to a lighting store that sells
+various lamps. But, she prefers explicitly an LED lamp that has a setting to change
+different light colors. 
 """
 
 class Lamp:
@@ -355,6 +422,14 @@ class Lamp:
 
     def change_state(self,st):
         self.state = st
+	
+"""
+The store assistant brought her two brands of LED lamps in the name of Sylphat and
+Garimond. First, the Sylphat brand is a bar-shaped LED lamp with a changing light
+color setting. So, she turned the lamp on, checked the white light color, and then
+turned it off to see if it fit her bedroom well. Meanwhile, the Garimond brand was
+circular, but she kept it turned off. 
+"""
         
 # This is the Main Program
 
@@ -382,12 +457,21 @@ print ("Brand Name: ",lamp2.name)
 print ("Shape: ", lamp2.shape)
 print ("Lamp state: ", lamp2.state)
 
+"""
+In the end, she made a quick decision and bought the Sylphat LED lamp as
+she found the style more pleasing and the light friendly to her vision.
+"""
+
+
+#We will now create a new class.
+#We will make a Refrigerator object!
 
 """
 Situation:
-Josanine wanted to contribute into building the dream boarding place with her friends.
-She pitched to give a refrigirator but was confused with the options she was presented with during
-her shopping at the mall.
+Josanine went to the mall's appliance section to contribute to their group to find
+a refrigerator that best suited their needs at the boarding place. Specifically,
+she wants a side-by-side type of fridge with a capacity of 350 liters and
+preferably an LG brand. 
 """
 
 #We will now define the attributes and methods for the refrigerator object
@@ -418,7 +502,15 @@ class Refrigerator:
         
     def set_temperature(self,temp):
         self.temperature = temp
-        
+
+	
+"""
+Fortunately, the store has all the fridge features that she wants and the brand.
+She noticed that the fridge was already cool enough, even at its current
+temperature of 24 degrees Celcius. However, she wanted another color option besides
+the Shiny Silver color. So, the store assistant offered her a refrigerator with the
+same features but in the shade of Midnight Black.
+"""
 #And this is the Main Program
 
 #This weird lines are here to act as a decoration.
@@ -452,7 +544,11 @@ print ("State:", refA.state)
 print ("Current Temp.: ", refA.temperature)
 
 
-#And so, Josanine was delighted with her newly bought refrigerator in "Midnight Black!"
+"""
+At last, Josanine found the LG refrigerator in Midnight Black, the one for their
+new boarding place. So she went ahead to order it and have it delivered to their
+home.
+"""
 
 
 
@@ -461,11 +557,19 @@ print ("Current Temp.: ", refA.temperature)
 
 """
 Situation:
-Josanine headed to the "Washing Section" of the appliance store she visited yesterday.
-She was greeted with vast variety of irons to pick, so she decided to narrow down her choices
-into three brands offering similar functionalities (her trusted brand "Xiaomi", and two unfamiliar
-brands she saw while browsing the catalouge online).
-She wanted an iron that will help her save space in her room.
+Another day, another chance for Josanine to finish her shopping errand. So, she
+returned to the mall's Appliance Section to find an electric iron for her clothes.
+She was greeted with a wide variety of choices. So, she narrowed down her choices
+by picking three brands offering similar functionalities: Xiaomi, her trusted
+brand, and two other unfamiliar brands in the name of Black+Decker and Rowenta
+Focus. In addition, she wants something that will save space in her room. 
+
+So, since no store assistants were available to assist her, she could not turn the
+items on. But she decided based on the features included in each brand she picked:
+
+The Black+Decker was in color black, a steam iron, with 1,200 watts.
+The Xiaomi travel steam iron in color white with 1,200 watts.
+The Rowenta Focus, which is in the color red, is steam iron with 1,725 watts. 
 """
 
 #We will now define the attributes and methods of the iron object
@@ -532,14 +636,26 @@ print ("Type: ", iron3.type)
 print ("Wattage: ",iron3.wattage)
 print ("State:", iron3.state)
 
-#Josanine was happy for her purchase!
-#Josanine picked the Xiaomi Travel Steam Iron as it was compact and has a small form-factor.
+"""
+In the end, Josanine purchased the  Xiaomi travel steam iron as it is an absolute
+space saver and is already a familiar and trusted brand to her.
+"""
+
+#We will now create a new class.
+#The class will be called "Slippers" as we want to create a slipper object.
 
 """
-Josanine suddenly decided to pick a new slippers to use for inside and for outside of the house.
-She was presented with various options by the sales lady inside the "Clothing Section" of the mall
-she went in.
-So she narrowed down her options by looking at specific brands and specific designs.
+Situation:
+One more thing from Josanine's list is a new pair of slippers for inside and
+outside the house. So, she went to the mall's Footwear Section, overwhelmed with
+how many choices she had to choose from. 
+
+She found three brands to choose from: the Marquina - The Bonnie Penny Loafers
+office leather shoes in an oxblood burgundy color size seven. Then, she found the
+Nike - Jordan Sophia indoor rubber and foam slides in an off-white color available
+in size seven. And the Vionic - Mule Slippers indoor slippers in a polyester terry
+cloth material in the colors tan, black, light pink, and dark blue are available in
+sizes eight.
 """
 
 
@@ -577,6 +693,8 @@ class Slippers:
 print("")
 print("")
 
+
+#We will now create three objects out of the defined attributes and methods above
 slip1 = Slippers("Marquina - The Bonnie Penny Loafers","Oxblood Burgundy","7","Office Shoes","Leather  ")
 slip2 = Slippers("Nike - Jordan Sophia","Off-White","7","INDOORS","Rubber and Foam")
 slip3 = Slippers("Vionic - Mule Slippers","Tan","8","INDOORS","Polyester Terry Cloth")
@@ -615,11 +733,9 @@ print ("Material Used:", slip3.material)
 
 
 """
-Josanine was feeling tired from trying out various shoe types and sizes.
-She only looked for one outdoor shoe and two for indoor shoes.
-Josanine then decided to purchase one outdoor and one indoor shoe.
-And she decided to go for her only option for outdoor shoes and her most loved mule indoors shoes
-from Vionic.
+After a while, Josanine finally chose the Marquina brand office leather shoes for
+her outdoor pair, as it was her only choice. And bought the Vionic brand for her
+indoor pair of slippers.
 """
 
 
